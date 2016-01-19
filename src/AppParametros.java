bd569b1d9392681d060bfd473d28a1d99185800e
Codizer <adrianortizmartinez@gmail.com>
2016-01-19 01:47:15 -0600

    Create project 06-ParametrosVentaja
    This project has Docs and Diagram

---
+++ src/AppParametros.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author Adrian
+ *
+ */
+public class AppParametros {
+
+	/**
+	 * Aplicación que calcula operaciones básicas de trígonometria
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Ventana v = new Ventana();
+		v.setVisible(true);
+	}
+}
